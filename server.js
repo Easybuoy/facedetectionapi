6469b2a16c8c3fc565492601f7daae9bc3bec397
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-05-24 19:11:12 +0100

    [feature] facedetectionapi

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post('/register', (req, res) => {
             .then((user) =>{
 
                 delete user.password;
-                // sendmail(from_email, to_email, subject, content);
+                sendmail(from_email, to_email, subject, content);
                 res.json(user);
             })
             .catch(err => res.status(502).json("Unable To Register User. Please Try Again"));
@@ -137,43 +137,43 @@ app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
 
-// const sendmail = (from_email, to_email, subject, content) => {
-//     var helper = require('sendgrid').mail;
-// var from_email = new helper.Email(from_email);
-// var to_email = new helper.Email(to_email);
-// var content = new helper.Content('text/plain', content);
-// var mail = new helper.Mail(from_email, subject, to_email, content);
+const sendmail = (from_email, to_email, subject, content) => {
+    var helper = require('sendgrid').mail;
+var from_email = new helper.Email(from_email);
+var to_email = new helper.Email(to_email);
+var content = new helper.Content('text/plain', content);
+var mail = new helper.Mail(from_email, subject, to_email, content);
 
-// // var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 // var sg = require('sendgrid')('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');
-// var request = sg.emptyRequest({
-//   method: 'POST',
-//   path: '/v3/mail/send',
-//   body: mail.toJSON(),
-// });
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
 
-// sg.API(request, function(error, response) {
-//     var request = {
-//         from_email: from_email,
-//         to_email: to_email,
-//         subject: subject,
-//         content: content
-//     }
-//     var request = JSON.stringify(request);
-//     var response = JSON.stringify(response);
+sg.API(request, function(error, response) {
+    var request = {
+        from_email: from_email,
+        to_email: to_email,
+        subject: subject,
+        content: content
+    }
+    var request = JSON.stringify(request);
+    var response = JSON.stringify(response);
 
-//     const mailLog = {
-//         request: request,
-//         request_time: new Date(),
-//         response: response,
-//         response_time: new Date()
-//     }
-//     new MailLog(mailLog).save()
-//     .then((res) => {
-//         return res;
-//     })
-//     .catch((err) => {
-//         return err;
-//     });
-// });
-// }
+    const mailLog = {
+        request: request,
+        request_time: new Date(),
+        response: response,
+        response_time: new Date()
+    }
+    new MailLog(mailLog).save()
+    .then((res) => {
+        return res;
+    })
+    .catch((err) => {
+        return err;
+    });
+});
+}
